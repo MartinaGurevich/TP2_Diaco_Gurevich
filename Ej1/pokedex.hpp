e4a073b9180a73608bba02e5e11ddbe333491430
Abril Diaco <adiaco@udesa.edu.ar>
2025-05-28 15:39:06 -0300

    inicios clases

---
+++ Ej1/pokedex.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <unordered_map>
+
+class Pokedex{
+    private:
+        std::unorder_map<Pokemon, PokemonInfo> info; //completar
+    
+    public:
+        //constructor
+        Pokedex() = 
+        Pokedex(const std::string& file); //sobrecarga de sontructor para serializar
+
+        //sobrecarga del Hash
+        //ver si pasar a privado
+
+        namespase std ////ahora lo vep con martu
+
+        //metodos
+        void agregarPokemon(const Pokemon& pokemon, const PokemonInfo& info);
+        void mostrar(const Pokemon& pokemon) const; //ver si se pasa por parametro
+        void mostrarTodos() const;
+
+}
