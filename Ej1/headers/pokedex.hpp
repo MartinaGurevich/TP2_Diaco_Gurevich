3c08510f617205ada6b0d0aa76e7c8a28252cf8e
Abril Diaco <adiaco@udesa.edu.ar>
2025-05-28 16:09:33 -0300

    listo pokedex

--- Ej1/headers/pokedex.hpp
+++ Ej1/headers/pokedex.hpp
@@ -1,24 +1,33 @@
 #pragma once
 #include <unordered_map>
+#include "pokemon.hpp"
 
+//funcion de Hash para el nombre del Pokemon implementada como functor
+
+class PokemonHash{
+    public:
+        std::size_t operator()(const Pokemon& pokemon) const {
+            return std::hash<std::string>()(pokemon.getName());
+        }
+};
 
 class Pokedex{
     private:
-        std::unorder_map<Pokemon, PokemonInfo> info; //completar
+        std::unorder_map<Pokemon, PokemonInfo, PokemonHash> info; //completar
     
     public:
         //constructor
-        Pokedex() = 
+        Pokedex() = default;
         Pokedex(const std::string& file); //sobrecarga de sontructor para serializar
  
-        //sobrecarga del Hash
-        //ver si pasar a privado
-
-        namespase std ////ahora lo vep con martu
-
+        
         //metodos
         void agregarPokemon(const Pokemon& pokemon, const PokemonInfo& info);
+        void eliminarPokemon(const Pokemon& pokemon);
         void mostrar(const Pokemon& pokemon) const; //ver si se pasa por parametro
         void mostrarTodos() const;
-
-}
+        
+        void serializar(const std::string& file) const; //serializa el pokedex a un archivo
+        void deserializar(const std::string& file); //deserializa el pokedex desde un archivo
+        
+};
