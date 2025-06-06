cb6048cda201d9097da033de763543a7ada60408
Abril Diaco <adiaco@udesa.edu.ar>
2025-06-06 17:20:59 -0300

    avances el 2

--- Ej2/examples/main.cpp
+++ Ej2/examples/main.cpp
@@ -1,26 +1,30 @@
-#include "../includes/drone.hpp"
+#include "../headers/drone.hpp"
 #include <thread>
+#include <memory>
 using namespace std;
 
 int main(){
 
-    Drone drone1("Drone1");
-    Drone drone2("Drone2");
-    Drone drone3("Drone3");
-    Drone drone4("Drone4");
-    Drone drone5("Drone5");
+    unique_ptr<Drone> drone1 = make_unique<Drone> ("Drone1");
+    unique_ptr<Drone> drone2 = make_unique<Drone> ("Drone2");
+
+    // Drone drone1("Drone1");
+    // Drone drone2("Drone2");
+    // Drone drone3("Drone3");
+    // Drone drone4("Drone4");
+    // Drone drone5("Drone5");
    
-    vector<Drone> drones;
-    drones.push_back(drone1);
-    drones.push_back(drone2);
-    drones.push_back(drone3);
-    drones.push_back(drone4);
-    drones.push_back(drone5);
+    vector<unique_ptr<Drone>> drones;
+    drones.push_back(std::move(drone1));
+    drones.push_back(std::move(drone2));
+    // drones.push_back(drone3);
+    // drones.push_back(drone4);
+    // drones.push_back(drone5);
 
     vector<std::jthread> threads;
 
-    for (int i = 0; i<5; i++){
-        threads.emplace_back(volar, drones[i], i);
+    for (int i = 0; i<2; i++){
+        threads.emplace_back(volar, ref(drones[i]), i);
     }
 
     // for(auto& t: threads){
--- Ej2/src/drone.cpp
+++ Ej2/src/drone.cpp
@@ -1,31 +1,32 @@
-#include "../includes/drone.hpp"
+#include "../headers/drone.hpp"
 using namespace std;
 
+Drone::Drone(const string& nombre) : nombre(nombre) {}
 
 std:: mutex m2; //para los cout
 
-void volar(Drone& drone, vector<int> disponibilidad, int i){
+void volar(unique_ptr<Drone> drone, vector<int> disponibilidad, int i){
     while(true){
 
-        drone.m1.lock(); //accede al mutex de struct Drone
+        drone->m1.lock(); //accede al mutex de struct Drone
         if(i==0){
             if(disponibilidad[-1] == 1 && disponibilidad[i+1] == 1){
                 while(true){
                     if(m2.try_lock()){
-                        cout<<drone.nombre<<" despegando..."<<endl;
+                        cout<<drone->nombre<<" despegando..."<<endl;
                         disponibilidad[i]= 0;
                         m2.unlock();
                         this_thread::sleep_for(chrono::seconds(5));
                         disponibilidad[i]= 1;
                         while(true){
                             if(m2.try_lock()){
-                            cout<<drone.nombre<<" Altura de 10 metros."<<endl;
+                            cout<<drone->nombre<<" Altura de 10 metros."<<endl;
                             m2.unlock();
                             break;
                             }
                         break;
                         }
-                    } else cout<<drone.nombre<<" esperando para despegar... "<<endl;
+                    } else cout<<drone->nombre<<" esperando para despegar... "<<endl;
             }
         }
     }
@@ -33,40 +34,40 @@ void volar(Drone& drone, vector<int> disponibilidad, int i){
             if(disponibilidad[i-1] == 1 && disponibilidad[0] == 1){  
                 while(true){
                     if(m2.try_lock()){
-                        cout<<drone.nombre<<" despegando..."<<endl;
+                        cout<<drone->nombre<<" despegando..."<<endl;
                         disponibilidad[i]= 0;
                         m2.unlock();
                         this_thread::sleep_for(chrono::seconds(5));
                         disponibilidad[i]= 1;
                         while(true){
                             if(m2.try_lock()){
-                            cout<<drone.nombre<<" Altura de 10 metros."<<endl;
+                            cout<<drone->nombre<<" Altura de 10 metros."<<endl;
                             m2.unlock();
                             break;
                             }
                         break;
                         }
-                    } else cout<<drone.nombre<<" esperando para despegar... "<<endl;
+                    } else cout<<drone->nombre<<" esperando para despegar... "<<endl;
             } 
         }
     }
         if(disponibilidad[i-1] == 1 && disponibilidad[i+1] == 1){
             while(true){
                 if(m2.try_lock()){
-                    cout<<drone.nombre<<" despegando..."<<endl;
+                    cout<<drone->nombre<<" despegando..."<<endl;
                     disponibilidad[i]= 0;
                     m2.unlock();
                     this_thread::sleep_for(chrono::seconds(5));
                     disponibilidad[i]= 1;
                     while(true){
                         if(m2.try_lock()){
-                        cout<<drone.nombre<<" Altura de 10 metros."<<endl;
+                        cout<<drone->nombre<<" Altura de 10 metros."<<endl;
                         m2.unlock();
                         break;
                         }
                     break;
                     }
-                } else cout<<drone.nombre<<" esperando para despegar... "<<endl;
+                } else cout<<drone->nombre<<" esperando para despegar... "<<endl;
             }
        
         }
